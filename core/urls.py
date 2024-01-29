1692e3f264e24e596678e148bc22f7a980953073
Sergio <sergio.pm.developer@gmail.com>
2024-01-29 01:26:50 +0000

    feat: add login page and integrate css in base.html

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from core.views import index
 
 urlpatterns = [
     path("", index, name="index"),
+    path("login", auth_views.LoginView.as_view(), name="login"),
 ]
