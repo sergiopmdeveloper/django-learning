a42bffd6d1501e3b5bae8d6691fdfcc405e8d30b
Sergio <sergio.pm.developer@gmail.com>
2024-02-01 02:03:40 +0000

    feat: implement sign up service and validation

--- core/forms.py
+++ core/forms.py
@@ -18,3 +18,41 @@ class LoginForm(forms.Form):
             "required": "Required",
         },
     )
+
+
+class RegisterForm(forms.Form):
+    """
+    Register form validator
+    """
+
+    username = forms.CharField(
+        required=True,
+        error_messages={
+            "required": "Required",
+        },
+    )
+    first_name = forms.CharField(
+        required=True,
+        error_messages={
+            "required": "Required",
+        },
+    )
+    last_name = forms.CharField(
+        required=True,
+        error_messages={
+            "required": "Required",
+        },
+    )
+    email = forms.EmailField(
+        required=True,
+        error_messages={
+            "required": "Required",
+            "invalid": "Invalid email",
+        },
+    )
+    password = forms.CharField(
+        required=True,
+        error_messages={
+            "required": "Required",
+        },
+    )
--- core/views.py
+++ core/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
 
 
 @login_required()
@@ -72,6 +74,74 @@ def sign_up(request: HttpRequest) -> HttpResponse:
 
     if request.user.is_authenticated:
         return redirect("/")
+
+    form_errors = {}
+
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
+
+            try:
+                new_user = User.objects.create_user(
+                    username=username,
+                    first_name=first_name,
+                    last_name=last_name,
+                    email=email,
+                    password=password,
+                )
+
+            except IntegrityError:
+                return render(
+                    request,
+                    "registration/register.html",
+                    {
+                        "user_already_exists": True,
+                    },
+                )
+
+            new_user.save()
+
+            user = authenticate(username=username, password=password)
+
+            if user:
+                login(request, user)
+
+                return redirect("/")
+
+            else:
+                return render(
+                    request,
+                    "registration/register.html",
+                    {
+                        "register_error": True,
+                    },
+                )
+
+        else:
+            form_errors["username"] = form.errors.get("username", [None])[0]
+            form_errors["first_name"] = form.errors.get("first_name", [None])[0]
+            form_errors["last_name"] = form.errors.get("last_name", [None])[0]
+            form_errors["email"] = form.errors.get("email", [None])[0]
+            form_errors["password"] = form.errors.get("password", [None])[0]
+
+            return render(
+                request,
+                "registration/register.html",
+                {
+                    "form_errors": form_errors,
+                    "username": request.POST["username"],
+                    "first_name": request.POST["first_name"],
+                    "last_name": request.POST["last_name"],
+                    "email": request.POST["email"],
+                    "password": request.POST["password"],
+                },
+            )
 
     return render(request, "registration/register.html")
 
